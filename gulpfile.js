e405db0f4c2d2e7169016654e0018b8d45f64214
Giuseppe <giuseppe.galati84@gmail.com>
2017-02-14 17:43:03 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,47 @@
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+var sass = require (`gulp-sass`);
+var htmlmin = require ('gulp-htmlmin');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
 });
+
+gulp.task('sass:watch', function () {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('minify', function() {
+  return gulp.src('app/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task('default', ['serve']);
